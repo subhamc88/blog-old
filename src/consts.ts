58d1dae3c6659a26d8e37506a1840c64304c85da
subhamc88 <subham020304@gmail.com>
2024-09-12 21:04:23 +0530

    title changed

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'SUBHAMC88\'s blog';
+export const SITE_TITLE = 'subhamc88\'s blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
